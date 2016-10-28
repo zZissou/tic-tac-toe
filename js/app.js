660990e3acfcee6af9b1331f4bd2426500e49096
zZissou <kyle.edward.russell@gmail.com>
2016-10-28 16:28:58 -0700

    added reset button to js, added boxes to html.

--- js/app.js
+++ js/app.js
@@ -3,4 +3,30 @@ $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
 
+//store id when clicked,
+  //create for function store click as i when i = 0 i++ if odd, its adds X text to id. If even, adds Y text to id.
+
+
+  // for (var i = 0; i < 10; i++) {
+  //   .onclick
+  //   if #box has text {
+  //     "DON'T LET IT WORK"
+  //   } else if (i % 2 === 0) {
+      // $('#box1').on('click', function () {
+      //   $(this).text('X');
+    // } else {
+    //   $('#box1').on('click', function () {
+    //     $(this).text('O');
+    // }
+
+
+
+
+  $('#box1').on('click', function () {
+    $(this).text('X');
+  });
+
+  $('.btn').click(function() {
+    location.reload();
+  });
 });
