81be2ed21e97d857c8c994fe690d0cfbd1727070
zZissou <kyle.edward.russell@gmail.com>
2016-11-05 19:15:36 -0700

    fixed JS file. had an extra }).

--- js/app.js
+++ js/app.js
@@ -61,15 +61,16 @@ $(document).ready(function() {
 
   var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
 
-$(document).keydown(function(e) {
+  $(document).keydown(function(e) {
 
-  kkeys.push( e.keyCode );
+    kkeys.push( e.keyCode );
 
-  if ( kkeys.toString().indexOf( konami ) >= 0 ) {
+    if ( kkeys.toString().indexOf( konami ) >= 0 ) {
 
-    $(document).unbind('keydown',arguments.callee);
+      $(document).unbind('keydown',arguments.callee);
 
-    // do something awesome
-    $('.box').addClass("konami");
-  }
+      // do something awesome
+      $('.box').addClass("konami");
+    }
+  });
 });
